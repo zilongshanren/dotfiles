9ce026963976d0cc9633da1d2fc3ab2b19e94729
lion <254041321@qq.com>
2012-12-05 19:32:41 +0800

    add install scripts

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Install vim configurations..."
+
+echo "cd to .vim directory"
+cd ~/.vim
+
+echo "remove lua-ftplugin to prevent doc error msg"
+
+rm bundle/vim-lua-ftplugin/doc/lua-ftplugin.txt
+
+echo "create .vimrc"
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/vimrc ~/.gvimrc
+
+echo "copy markdown.pl to /usr/local/bin  directory"
+cp Markdown.pl /usr/local/bin
+
+echo "update submodules"
+git submodule init
+git submodule update
