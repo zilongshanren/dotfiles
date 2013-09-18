305534915a1d88498d6822f64fbb5d69980a6b9d
andyque <guanghui8827@126.com>
2013-09-18 09:28:17 +0800

    modify config

---
+++ .emacs.d/elpa/evil-20130824.1123/evil-pkg.el
@@ -0,0 +1,3 @@
+(define-package "evil" "20130824.1123" "Extensible Vi layer for Emacs. [git]"
+  '((undo-tree "0.6.3")))
+
---
+++ .emacs.d/elpa/org-20130909/org-pkg.el
@@ -0,0 +1,3 @@
+(define-package "org"
+  "20130909" "Outline-based notes management and organizer" ( ))
+;; no-byte-compile: t
